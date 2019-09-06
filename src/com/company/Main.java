1705134364f062e24e1167648f412651d15b837b
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-06 20:28:40 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+// посмотри внимательнее документ "Как выполнять ДЗ". -> homework_1.task_1.Task_1.java вот так должно быть
 public class Main {
 
     public static void main(String[] args) {
