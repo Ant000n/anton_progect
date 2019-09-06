ec048b491c3a52976db21736506943de48a21c40
Anton <ant00n.93@mail.ru>
2019-09-06 16:58:35 +0300

    commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Приветствую пользователя. Имя пользователя передать аргументом программы");
+    }
+}
