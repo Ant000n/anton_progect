6d4c8dcb83978443cb51167867e49d444302d93f
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-07 10:39:30 +0300

    Update Task_1.java

--- src/com/company/homework_1/task_1/Task_1.java
+++ src/com/company/homework_1/task_1/Task_1.java
@@ -1,5 +1,6 @@
 package com.company.homework_1.task_1;
 
+// Это была Task_5 + вывод на экран + передать аргументы программе
 public class Task_1 {
     public static void main(String[] args) {
     }
