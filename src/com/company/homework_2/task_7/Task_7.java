8a046d7f7a15dfc0c5ce705d267e51f1be1aa6f2
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-11 22:13:36 +0300

    Update Task_7.java

--- src/com/company/homework_2/task_7/Task_7.java
+++ src/com/company/homework_2/task_7/Task_7.java
@@ -2,9 +2,9 @@ package com.company.homework_2.task_7;
 
 public class Task_7 {
     public static void main(String[] args) {
-
+// пустая строка не нужна
         double a = 8.0;
-
+// пустая строка не нужна
         double b = 2.5;
 
         double c = a / b;
