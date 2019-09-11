8679f77304bff8fe68225591b084a4662263db85
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-11 21:58:32 +0300

    Update Task_1.java

--- src/com/company/homework_2/task_1/Task_1.java
+++ src/com/company/homework_2/task_1/Task_1.java
@@ -5,8 +5,11 @@ public class Task_1 {
     public static void main(String[] args) {
 
         int myInt = 5;
-
+//         double myDouble = myInt; - когда идет повышение типа нету необходимости делать явное приведение типа
+        
         double myDouble = (double) myInt;
+        
+        // а теперь надо myInt = myDouble сделать
 
         System.out.println("myDouble = " + myDouble);
     }
