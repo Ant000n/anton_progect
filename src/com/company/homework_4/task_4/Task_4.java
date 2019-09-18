dd698269941184152eef81cc0935b03b02295039
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-18 21:52:46 +0300

    Update Task_4.java

--- src/com/company/homework_4/task_4/Task_4.java
+++ src/com/company/homework_4/task_4/Task_4.java
@@ -2,6 +2,7 @@ package com.company.homework_4.task_4;
 
 public class Task_4 {
     public static void main(String[] args) {
+        // task_4 -> text
         String task_4 = "While major gameplay components are already in place and functioning,\n" +
                 "players help is needed to add some of the smaller features and content.\n" +
                 "Of course polishing the overall experience is important at the same time.\n" +
@@ -12,5 +13,7 @@ public class Task_4 {
         System.out.println("Есть ли слово 'add' в тексте? = " + task_4.contains("add"));
         System.out.println("Есть ли слово 'good' в тексте? = " + task_4.contains("good"));
         System.out.println("Есть ли слово 'plan' в тексте? = " + task_4.contains("plan"));
+        
+        // осталось только посчитать кол-во повторений
     }
 }
