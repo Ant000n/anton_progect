0fa724511e16783096a32f1e61eb6e3a2bd56b9a
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-18 21:51:49 +0300

    Update Task_3.java

--- src/com/company/homework_4/task_3/Task_3.java
+++ src/com/company/homework_4/task_3/Task_3.java
@@ -2,6 +2,7 @@ package com.company.homework_4.task_3;
 
 public class Task_3 {
     public static void main(String[] args) {
+        // task_3 -> sentence / words
         String task_3 = "Green. red. blue. yellow";
         System.out.println(task_3.replace(".",","));
     }
