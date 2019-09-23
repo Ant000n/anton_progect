4d9f18b61f9c1fa8d80973010e28e1284c36e86e
Anton <ant00n.93@mail.ru>
2019-09-23 19:12:56 +0300

    commit 1

--- src/com/company/homework_5/task_7/Task_7.java
+++ src/com/company/homework_5/task_7/Task_7.java
@@ -2,6 +2,27 @@ package com.company.homework_5.task_7;
 
 public class Task_7 {
     public static void main(String[] args) {
+        System.out.println("1                1");
+        System.out.println("12              21");
+        System.out.println("123            321");
+        System.out.println("1234          4321");
+        System.out.println("12345        54321");
+        System.out.println("123456      654321");
+        System.out.println("1234567    7654321");
+        System.out.println("12345678  87654321");
+        System.out.println("123456789987654321");
+        System.out.println("12345678  87654321");
+        System.out.println("1234567    7654321");
+        System.out.println("123456      654321");
+        System.out.println("12345        54321");
+        System.out.println("1234          4321");
+        System.out.println("123            321");
+        System.out.println("12              21");
+        System.out.println("1                1");
+
+
+
+
 
     }
 }
