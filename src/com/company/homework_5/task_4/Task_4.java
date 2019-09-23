235d243ccde1550758880b911f08c4b9a86ed4c1
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-23 20:41:02 +0300

    Update Task_4.java

--- src/com/company/homework_5/task_4/Task_4.java
+++ src/com/company/homework_5/task_4/Task_4.java
@@ -2,7 +2,7 @@ package com.company.homework_5.task_4;
 
 public class Task_4 {
     public static void main(String[] args) {
-
+// 6 лучше вынести в отдельную переменную
         for (int i = 6; i >= 0; i--) {
             for (int a = 0; a < i; a++) {
                 System.out.print(".");
