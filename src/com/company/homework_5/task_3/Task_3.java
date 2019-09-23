650ff242c1616a139a46d0009d6cda3d6d603f05
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-23 20:40:11 +0300

    Update Task_3.java

--- src/com/company/homework_5/task_3/Task_3.java
+++ src/com/company/homework_5/task_3/Task_3.java
@@ -2,6 +2,7 @@ package com.company.homework_5.task_3;
 
 public class Task_3 {
     public static void main(String[] args) {
+        // зачем перемення n, если ты ее не используешь?
         int n = 10;
         for (int i = 0; i < 10; i++) {
             System.out.println("You are welcome!");
