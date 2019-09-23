320e71042f70d0963f1d2cb8fb7228152008550c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-23 20:36:46 +0300

    Update Task_1.java

--- src/com/company/homework_5/task_1/Task_1.java
+++ src/com/company/homework_5/task_1/Task_1.java
@@ -3,11 +3,13 @@ package com.company.homework_5.task_1;
 
 public class Task_1 {
     public static void main(String[] args) {
+        // evenSum
         int sum = 0;
         for (int i = 0; i <= 100; i = i + 2) {
             sum = sum + i;
         }
         System.out.println("Сумма четных чисел: " + sum);
+        // sum_1 -> oddSum
         int sum_1 = 0;
         for (int x = 0; x <= 100; x++) {
             if (x % 2 != 0) {
@@ -15,6 +17,6 @@ public class Task_1 {
             }
         }
         System.out.println("Сумма нечетных чисел: " + sum_1);
-
+// пустая строка не нужна
     }
 }
