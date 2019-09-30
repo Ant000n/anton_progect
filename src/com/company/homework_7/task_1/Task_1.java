0f2d71039303ab8f73b421cdffc6d6a84ebb4344
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-30 21:09:00 +0300

    Update Task_1.java

--- src/com/company/homework_7/task_1/Task_1.java
+++ src/com/company/homework_7/task_1/Task_1.java
@@ -7,10 +7,12 @@ import java.io.IOException;
 public class Task_1 {
 
     private static final String PATH = "srs/com/company/homework_7/task_1/text";
+    
     public static void main(String[] args) {
         byte[] buf = {1, 2, 3, 4, 5, 6, 9};
        try(FileOutputStream fos = new FileOutputStream(PATH)) {
-
+           // здесь должен быть цикл с вызовом метода write
+            // fos.write()
        } catch (FileNotFoundException e) {
            e.printStackTrace();
        } catch (IOException e) {
