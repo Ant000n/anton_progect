c4bc0225e7e3f6eafe4cc1e0be3c5e0cdd62a716
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-30 21:14:59 +0300

    Update Task_4.java

--- src/com/company/homework_7/task_4/Task_4.java
+++ src/com/company/homework_7/task_4/Task_4.java
@@ -6,10 +6,13 @@ public class Task_4 {
     private static final String PATH = "src/com/company/homework_7/task_4/Employee";
     public static void main(String[] args) {
         Employee myEmployee = new Employee("Android", 2);
+        
         try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(PATH))) {
            oos.writeObject(myEmployee);
         } catch (Exception e) {
             e.printStackTrace();
+            
+            // не стоит в блоке catch что-то читать из файла. эта программа вряд ли вообще когда-либо отработает корректно
             try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(PATH))) {
                 final var employee = (Employee) ois.readObject();
             } catch (FileNotFoundException ex) {
