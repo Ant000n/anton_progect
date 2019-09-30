7c4e38b7f3ed2742c411a7e156932264a8556b95
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-30 21:13:19 +0300

    Update Employee.java

--- src/com/company/homework_7/task_4/Employee.java
+++ src/com/company/homework_7/task_4/Employee.java
@@ -2,11 +2,13 @@ package com.company.homework_7.task_4;
 
 import java.io.Serializable;
 
+// у тебя есть поле Work, а наследование здесь стоит убрать + наследование некорректно по отношению к Employee
 public class Employee extends Work implements Serializable {
     private String name;
     private int age;
     private Work work;
 
+    // public Employee(String title, int experience, Work work) - например так
     public Employee(String title, int experience) {
         super(title, experience);
     }
