2c4f206f703c845026a78a1475d8b3b1b01aeec7
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-07 20:40:23 +0300

    Update Task_2.java

--- src/com/company/homework_9/task_2/Task_2.java
+++ src/com/company/homework_9/task_2/Task_2.java
@@ -3,6 +3,7 @@ package com.company.homework_9.task_2;
 public class Task_2 {
 }
 
+// интерфейс должен быть с дженериком, и создай класс, который его имплементирует
 interface Wood {
     void showClassName();
 }
