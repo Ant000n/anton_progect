91fb070ed60814cb46eee688ecf5455120d211f8
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-14 21:13:23 +0300

    Update Task_4.java

--- src/com/company/homework_11/task_4/Task_4.java
+++ src/com/company/homework_11/task_4/Task_4.java
@@ -3,6 +3,7 @@ package com.company.homework_11.task_4;
 import java.util.ArrayList;
 import java.util.List;
 
+// +
 public class Task_4 {
     public static void main(String[] args) {
         List<String> words = new ArrayList<>();
