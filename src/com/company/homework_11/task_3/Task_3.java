022662f350fecc447da5d6834a7a66beccf9c5ff
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-14 21:11:32 +0300

    Update Task_3.java

--- src/com/company/homework_11/task_3/Task_3.java
+++ src/com/company/homework_11/task_3/Task_3.java
@@ -1,9 +1,11 @@
 package com.company.homework_11.task_3;
 
+// +
 public class Task_3 {
     public static void main(String[] args) throws InterruptedException {
         Store store = new Store(150);
 
+        // Producer
         ToysProbucer luntikToy = new ToysProbucer("luntik", store, 100);
         ToysProbucer cheburashkaToy = new ToysProbucer("cheburashka", store, 50);
 
