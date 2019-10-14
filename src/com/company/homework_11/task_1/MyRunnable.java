6a504864aa170f1161bef5103f2556f0ccc03983
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-14 20:56:19 +0300

    Update MyRunnable.java

--- src/com/company/homework_11/task_1/MyRunnable.java
+++ src/com/company/homework_11/task_1/MyRunnable.java
@@ -3,6 +3,7 @@ package com.company.homework_11.task_1;
 public class MyRunnable implements Runnable {
     @Override
     public void run() {
+        // 10 - лучше передавать в конструкторе
         for (int i = 0; i <= 10; i++) {
             System.out.println(i);
         }
